79de1dac914a2a4f42e6f8e3bfd26f111ee83027
tvenceslau <venceslau.tiago@gmail.com>
2026-05-25 18:43:36 +0100

    DECAF-798 - version

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,23 @@ export * from "./utils";
 export const VERSION = "##VERSION##";
 
 /**
+ * @description Represents the current commit hash of the module build.
+ * @summary Stores the current git commit hash for the package. The build replaces
+ * the placeholder with the actual commit hash at publish time.
+ * @const COMMIT
+ */
+export const COMMIT = "##COMMIT##";
+
+/**
+ * @description Represents the full version string of the module.
+ * @summary Stores the semver version and commit hash for the package.
+ * The build replaces the placeholder with the actual `<version>-<commit>` value at publish time.
+ * @const FULL_VERSION
+ */
+export const FULL_VERSION = "##FULL_VERSION##";
+
+
+/**
  * @description Stores the current package version.
  * @summary The version string of the for-typeorm package.
  * @const PACKAGE_NAME
